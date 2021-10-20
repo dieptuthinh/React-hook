e2848457e502a4db201303b375af6077bf7d041e
59132412 <dieptuthinh@gmail.com>
2021-10-20 17:49:54 +0700

    useState todolist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+// import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love Easy Frontend! 😍 " },
+    { id: 2, title: "We love Easy Frontend! 🥰 " },
+    { id: 3, title: "They love Easy Frontend! 🚀 " },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id)
+    if (index <0 ) {
+      return 0;
+    }
+    else {
+      const newTodoList = [...todoList];
+      newTodoList.splice(index, 1);
+      setTodoList(newTodoList)
+    }
+      
+  }
   return (
     <div className="app">
-      <h1>Hello</h1>
-
-      <ColorBox/>
+      <h1>Hello ReacJS</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick }/>
+      {/* <ColorBox/> */}
     </div>
   );
 }
